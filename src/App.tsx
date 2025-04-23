a618250bfb41d8f3dab46fdcd84765e3fc51c1c0
bogdan <bogdyrosut@gmail.com>
2025-04-23 04:41:17 -0700

    .map

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 function App() {
-  const [data, setData] = React.useState({ ip: '' , city: '' , country: '' , continent_code: '' , country_code: '' , region: '' , region_code: '' , latitude: '' , longitude: '' , timezone: '' , postal: '' , asn: '' , org: '' , carrier: '' , country_name: '' , continent_name: '' });
-  const handleTest = async () => {
+  const [data, setData] = React.useState("");
+  const handleApi = async () => {
     try {
       const response = await fetch('https://ipapi.co/json');
       const data = await response.json();
       setData(data);
+      console.log(data);
     }
     catch(error)
     {
@@ -14,25 +15,18 @@ function App() {
     }
   };
 
-  return (
-    <><button onClick={handleTest}>Button to show ip</button><div>
-      <ul>
-        <li>{data.ip}</li>
-        <li>{data.city}</li>
-        <li>{data.country}</li>
-        <li>{data.continent_code}</li>
-        <li>{data.country_code}</li>
-        <li>{data.region}</li>
-        <li>{data.region_code}</li> 
-        <li>{data.latitude}</li>
-        <li>{data.longitude}</li>
-        <li>{data.timezone}</li>
-        <li>{data.postal}</li>  
-        <li>{data.asn}</li>
-        <li>{data.org}</li>
-      </ul>
-    </div></>
-  );
+  return(
+    <div>
+      <h1>Api Test : </h1>
+      <button onClick={handleApi}>Press Api</button>
+      <div>
+        {Object.entries(data).map(([key , value]) => (
+          <li>{key} : {value}</li>
+            
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App;
